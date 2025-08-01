c25a87ab6fae570f875d8a78f42cedfa6f1bafc4
Mechstalol <141286968+Mechstalol@users.noreply.github.com>
2025-08-01 12:59:02 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Launch Flask app via Gunicorn on the port Azure probes (8000)
-exec gunicorn --bind 0.0.0.0:8000 --log-level debug bridge_app:app
+exec gunicorn --bind 0.0.0.0:${PORT:-8000} … bridge_app:app
